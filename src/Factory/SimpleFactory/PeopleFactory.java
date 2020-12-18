42853f305ede9b1d88f23c383c74e550799313e9
fear101 <zk1747793@163.com>
2020-12-18 13:24:11 +0800

    设计模式 简单工厂模式

---
+++ src/Factory/SimpleFactory/PeopleFactory.java
@@ -0,0 +1,25 @@
+package SimpleFactory;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author zengkan
+ * @Date: 2020/12/17/20:18
+ * @Description:这是一个工厂类型，主要是实现people的工厂
+ **/
+public class PeopleFactory {
+
+    /**
+     * createPeople主要通过反射的方式生成相应的类
+     * @return people返回生成的类
+     * */
+    public <T extends People> T createPeople(Class<T> c){
+        People people=null;
+        try {
+            people = (People) Class.forName(c.getName()).newInstance();
+        } catch (Exception e) {
+            System.out.println("性别选择错误");
+        }
+        return (T) people;
+    }
+}
---
+++ src/Factory/SimpleFactory/Program.java
@@ -0,0 +1,24 @@
+package SimpleFactory;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author zengkan
+ * @Date: 2020/12/17/20:01
+ * @Description:简单工厂的主函数
+ **/
+public class Program {
+
+    public static void main(String[] args) {
+        PeopleFactory peopleFactory = new PeopleFactory();
+
+        //男人的特征
+        People man= peopleFactory.createPeople(Man.class);
+        man.feature();
+
+        //女人的特征
+        People woman= peopleFactory.createPeople(Woman.class);
+        woman.feature();
+
+    }
+}
